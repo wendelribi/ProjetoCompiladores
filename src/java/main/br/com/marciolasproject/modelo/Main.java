be4b0f7bff9699da5a29be7e25d23e3937e631f2
Wendel Ribeiro <wendelribi@gmail.com>
2017-04-08 19:52:23 -0300

    Commit inicial do projeto Marciola

---
+++ src/java/main/br/com/marciolasproject/modelo/Main.java
@@ -0,0 +1,10 @@
+package br.com.marciolasproject.modelo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/java/main/br/com/marciolasproject/tela/Tela.java
@@ -0,0 +1,5 @@
+package br.com.marciolasproject.tela;
+
+public class Tela {
+
+}
