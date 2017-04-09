3ba39523fcfb21ff8cfa9fa8f3a98665aa4f7dcc
Wendel Ribeiro <wendelribi@gmail.com>
2017-04-08 21:20:22 -0300

    Tutorial

--- src/java/main/br/com/marciolasproject/modelo/Main.java
+++ src/java/main/br/com/marciolasproject/modelo/Main.java
@@ -7,11 +7,14 @@ public class Main {
 		// Depois que tiver feito a alteração e salvado:
 		// vai no direitorio do projeto
 		// verifica o que foi altrado com git status
-		// Depois adciona o que foi alterado para ser enviado: git add <nome do arquivo>
+		// Depois adciona o que foi alterado para ser enviado: git add <nome do
+		// arquivo>
 		// e depois envia: git commit -m "algo sobre a alteração"
 		// e atualiza o repositorio: git push
-		
 
+		String mulherMaisGataDoMundo = "Márcia Regina";
+
+		System.out.println(mulherMaisGataDoMundo);
 	}
 
 }
