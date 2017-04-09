8cd90e94cabaa6af9cc2494a9ae2203ec7e15797
Marcia <rcsilva.marcia@gmail.com>
2017-04-08 21:14:41 -0300

    fale alguma coisa sobre sua alteração

--- src/java/main/br/com/marciolasproject/modelo/Main.java
+++ src/java/main/br/com/marciolasproject/modelo/Main.java
@@ -3,7 +3,14 @@ package br.com.marciolasproject.modelo;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Alteração para exemplo
+		// Depois que tiver feito a alteração e salvado:
+		// vai no direitorio do projeto
+		// verifica o que foi altrado com git status
+		// Depois adciona o que foi alterado para ser enviado: git add <nome do arquivo>
+		// e depois envia: git commit -m "algo sobre a alteração"
+		// e atualiza o repositorio: git push
+		
 
 	}
 
